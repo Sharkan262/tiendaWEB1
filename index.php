14aa4d3dd53cba3a37667f96c5d0817fd4dbff99
Sharkan262 <mr.lesc262@gmail.com>
2020-10-19 17:03:13 -0500

    Version 1

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Styles/styles.css">
+    <link  rel="icon"   href="Images/favicon.png" type="image/png" />
+    <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet">
+    
+    <title>Tecno Store</title>
+</head>
+<body>
+    <div class="Contenedor">
+        <div class="navbar">
+            <ul>
+                <li>Tecno Store</li>
+                <li>nombre</li>
+                <li>casa</li>
+                <li>perro</li>
+                <li>carro</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
